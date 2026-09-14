d2a6a54b50fab66c54f11d89df77ea881b5a74d7
D62UCJ Hordósi Dorián <dorijan.hordosi@gmail.com>
2026-09-14 10:28:22 +0200

    ELSO SZINKRON

--- masodik.py
+++ masodik.py
@@ -1,2 +1,16 @@
 #A második labor feladatait tartlmataa
 print ("Szia")
+felhasznalokor = int(15.65)
+felhasznalokor += 1
+felhaszalonev = 'Józsi'
+felhaszalonev += 2
+jegyek = [2, 4, 5, 3]
+jegyek = [5]
+del jegyek[0]
+halmaz = {'magyar', 'angol', 'orosz', 3}
+elem = halmaz
+print(halmaz[0])
+hallgato ={"nev": "Jolán", "kor": 19}
+metszet = felhaszalonev[2]
+print(hallgato["nev"])
+print("Szia", felhaszalonev, '!', metszet)
